89956b83454e34c44549ca752e15ff62733fee4b
jto-d <jodonnell072@gmail.com>
2023-06-02 03:32:22 -0400

    baseline pendulum stuff

--- index.js
+++ index.js
@@ -9,20 +9,49 @@ const drawLine = (start, end) => {
     pen.stroke();
 }
 
+// Draw an arc (for visualization during testing)
+const drawArc = (x, y, radius, start, end) => {
+    
+    pen.beginPath();
+
+    pen.strokeStyle = "gray";
+    pen.arc(x, y, radius, start, end);
+
+    pen.stroke();
+
+}
+
 // Draw a circle (outline or filled)
-const drawCircle = (x, y, radius, start, end, action = "stroke") => {
+const drawCircle = (x, y, radius, action = "stroke") => {
 
     pen.beginPath();
 
-    pen.arc(x, y, radius, start, end)
+    pen.arc(x, y, radius, 0, 2 * Math.PI)
 
     if (action === "stroke") pen.stroke();
     else pen.fill();
 }
 
+const pendulum = (x, y, cirRadius, arcRadius) => {
+
+    // const velocity = 0.0005,
+    //     maxAngle = 2 * Math.PI
+    //     angle = Math.PI - (elapsedTime * velocity);
+    //     modAngle = angle % maxAngle;
+    //     angle = modAngle <= Math.PI && modAngle >= 0 ? modAngle : maxAngle - modAngle;
+    const gravity = 1
 
 
+    drawCircle(x, y, cirRadius)
+    drawArc(x, y, arcRadius, 0, Math.PI);
+}
+
+
+let startTime = new Date().getTime();
 const draw = () => {
+    const currTime = new Date().getTime(),
+        elapsedTime = (currTime - startTime)
+
     paper.width = paper.clientWidth;
     paper.height = paper.clientHeight;
 
@@ -46,11 +75,31 @@ const draw = () => {
     }
 
     pen.strokeStyle = "white";
-    pen.lineWidth = 0.5;
+    pen.lineWidth = 1;
+    pen.fillStyle = "white";
 
     drawLine(start, end);
     drawLine({x:0, y: paper.height * 0.05}, {x:paper.width, y: paper.height * 0.05});
+    
 
+    pen.lineWidth = 2;
+
+    const arcRadius = 500
+    pendulum(center.x, center.y, 40, arcRadius)
+
+    
+    drawCircle(center.x, center.y - 50, 40)
+    drawCircle(center.x, center.y - 100, 40)
+    drawCircle(center.x, center.y + 50, 40)
+    drawCircle(center.x, center.y + 100, 40)
+    drawCircle(center.x, center.y + 150, 40)
+    drawArc(center.x, center.y, 500, 0, Math.PI);
+
+    requestAnimationFrame(draw)
+
+
+
+    
 }
 
 draw();
